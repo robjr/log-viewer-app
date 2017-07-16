5ab88cb2dee56c8b6ba974ea8e069a8f0783ca8c
Roberto Júnior <me@robertojunior.net>
2017-07-16 20:13:23 -0300

    Show the file specified by the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LogService } from './shared/log.service';
+import { Line } from './shared/line.interface';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    public loadingFile: boolean;
+    public filePath: string;
+    public lines: Array<Line>;
+    public errorMessage: string;
+
+    private forwardReader: boolean;
+    private block: number;
+    private previousSubscription: Subscription;
+
+    public constructor(private logService: LogService) {
+        this.loadingFile = false;
+        this.filePath = '';
+        this.forwardReader = true;
+        this.block = 0;
+        this.lines = [];
+        this.previousSubscription = new Subscription();
+    }
+
+    public activateForwardReader() {
+        this.forwardReader = true;
+        this.block = 0;
+        this.readFile();
+    }
+
+    public deactivateForwardReader() {
+        this.forwardReader = false;
+        this.block = 0;
+        this.readFile();
+    }
+
+    public readFile() {
+        if ('' === this.filePath) {
+            return;
+        }
+
+        this.errorMessage = '';
+        this.loadingFile = true;
+        this.lines = [];
+        this.previousSubscription.unsubscribe();
+
+        this.previousSubscription = this.logService.readFile(this.forwardReader, this.filePath, this.block).subscribe(
+            (lines: Array<Line>) => {
+                this.lines = lines;
+                this.loadingFile = false;
+            },
+            error => {
+                if (error['code'] === 0) {
+                    this.block--;
+                } else {
+                    this.errorMessage = error['message'];
+                }
+                this.loadingFile = false;
+            }
+        );
+    }
+
+    public nextBlock() {
+        if (this.forwardReader) {
+            this.block++;
+        } else if (this.block > 0) {
+            this.block--;
+        }
+
+        this.readFile();
+    }
+
+    public previousBlock() {
+        if (!this.forwardReader) {
+            this.block++;
+        } else if (this.block > 0) {
+            this.block--;
+        }
+
+        this.readFile();
+    }
 }
